2b980d2fc55acb494375d485a9182b7f7e1a795f
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-04-09 10:44:48 +0300

    update the about page and the footer of the website

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -14,9 +14,9 @@ import Footer from '@/components/FooterComponent.vue'
         <div class="about_container">
             <div class="about_content">
                 <div class="about_items">
-                    <p>Welcome to LawNet Tour and Travel! Founded in 2022, we specialize in providing exceptional legal-themed virtual reality experiences.
-                        Our immersive simulations and educational content redefine legal education and entertainment. Join us as we lead the way in shaping 
-                        the future of legal VR. Embark on an unforgettable journey with LawNet.
+                    <p>Founded in 2022, Lanwnet Tours and Travel started as a humble family venture and swiftly burgeoned into a global entity.
+                        Specializing in personalized and immersive travel experiences, it has become synonymous with exceptional service and unforgettable
+                        adventures across the globe.
                     </p>
                 </div>
                 <div class="about_image">
@@ -26,14 +26,14 @@ import Footer from '@/components/FooterComponent.vue'
 
             <h2>Our Founder</h2>
             <div class="founder">
-                <p>Lawrence, the visionary owner of LawNet Tour and Travel, is a dedicated technologist with a passion for pushing the boundaries of virtual reality.
-                    With a background in law and a keen interest in technology, Lawrence brings a unique perspective to the company, driving innovation and excellence 
-                    in every aspect of its operations.
+                <p>Lawrence, founder of Lanwnet Tours and Travel, ignited his passion for exploration early on. Starting from scratch in 2022,
+                    his vision transformed into a global enterprise, driven by a commitment to deliver personalized and memorable travel
+                    experiences worldwide.
                 </p>
                 <div class="founder_image">
                     <img src="../assets/images/image1.jpg" alt="Lawrence Image">
                     <div class="infor">
-                        <p>Founder & CEO (lawnet Tours & Travel)</p>
+                        <p>Founder & CEO <span> (Lawnet Tours & Travel)</span></p>
                         <p>Lawrence Mwangi</p>
                     </div>
                 </div>
