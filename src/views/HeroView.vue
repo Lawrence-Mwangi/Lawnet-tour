46c06488926ddcf4c2308c2fe1d8e495ac87eef6
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-04-09 11:02:40 +0300

    Added an animation to the hero section

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -3,7 +3,7 @@
 <template>
     <div class="hero_container">
         <div class="hero_content">
-            <h1>Unlock your next adventure and venture beyond the ordinary &#127757;</h1>
+            <h1>Unlock your next adventure and venture beyond the ordinary <span class="rotate">&#127757;</span></h1>
             <p>Discover new horizons with us and get to know new places.</p>
 
             <div class="btn">
